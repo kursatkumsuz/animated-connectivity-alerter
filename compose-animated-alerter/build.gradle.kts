a681561e437b98acac1be9fd1d8b12dda7ee9d20
Kürşat Kumsuz <kursatkumsuz@ogr.iu.edu.tr>
2024-08-11 18:47:35 +0300

    Implement jitpack for release

--- compose-animated-alerter/build.gradle.kts
+++ compose-animated-alerter/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    `maven-publish`
+
 }
 
 android {
@@ -38,9 +40,36 @@ android {
     }
 }
 
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.kursatkumsuz"
+            artifactId = "animated-connectivity-alerter"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components.getByName("release"))
+            }
+
+            pom {
+                name.set("Animated Connection Alerter")
+                description.set("A library for animated connection alerts in Jetpack Compose")
+                url.set("https://github.com/kursatkumsuz/animated-connectivity-alerter")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/kursatkumsuz/animated-connectivity-alerter")
+                }
+            }
+        }
+    }
+}
+
 dependencies {
-
-
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.material3)
     implementation(libs.constraintLayout)
